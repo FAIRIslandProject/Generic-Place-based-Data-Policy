ebc6ff7fcfc7ff8da6af4785eaa1a7db8a29b04e
Erin Robinson <erinmr@gmail.com>
2022-07-26 16:25:38 -0600

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,65 +1,63 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
+# -- Project information
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
+project = 'FAIR Island Generic Data Policy'
+author = 'FAIR Island Project'
 
+release = '1.0.0'
+version = '1.0.0'
 
-# -- Project information -----------------------------------------------------
+# -- General configuration
 
-project = 'Generic Place Based Data Policy'
-copyright = '2022, FAIR Island Project Team'
-author = 'FAIR Island Project Team'
-
-# The short X.Y version
-version = "1.0.0"
-
-# The full version, including alpha/beta/rc tags
-release = 'V1.0.0'
-
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-   "sphinx_comments"
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx_comments'
 ]
 
-
-comments_config = {
-    "hypothesis": True
-    
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+intersphinx_disabled_domains = ['std']
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+html_logo = "_static/DataCite-Logo_secondary-white.png"
+
+html_theme_options = {
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': True
+}
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
 
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-# html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
+
+comments_config = {
+   "utterances": {
+      "repo": "github-org/github-repo",
+      "optional": "config",
+   }
+}
